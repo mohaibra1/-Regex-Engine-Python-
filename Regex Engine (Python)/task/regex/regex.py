411221642a8869d3d4343f1c61c4561e47fadbb7
Moha <mimohadinho@gmail.com>
2024-11-18 15:29:54 +0100

    Recursion regex function

--- Regex Engine (Python)/task/regex/regex.py
+++ Regex Engine (Python)/task/regex/regex.py
@@ -1,3 +1,4 @@
+
 def regex(reg): # write your code here
     reg = reg.split('|')
     if reg[1] == '' and reg[0] != '':
@@ -8,5 +9,31 @@ def regex(reg): # write your code here
         return False
 
     return True
+
+def recursion_regex(reg):
+    str_reg, reg = process_regex(reg)
+    temp = regex(str_reg)
+    if reg[0] == '|':
+        return temp
+    if temp:
+        return recursion_regex(reg)
+    return False
+
+def process_regex(reg):
+    first, second = reg.split('|')
+    first = [*first]
+    second = [*second]
+    if len(first) == 0:
+        first.append('')
+    if len(second) == 0:
+        second.append('')
+    str_reg = first[0] + '|' + second[0]
+    first.pop(0)
+    second.pop(0)
+    first = ''.join(first)
+    second = ''.join(second)
+    reg = first + '|' + second
+    return str_reg, reg
+
 regex_input = input()
-print(regex(regex_input))
+print(recursion_regex(regex_input))
